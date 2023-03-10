c37cec2d2f2f5722988516c89dd84046333d0884
andrericao <andrericao@hotmail.com>
2023-03-10 17:59:15 -0300

    .

--- Aleatorios/Javascript/contrutores/contrutores.js
+++ Aleatorios/Javascript/contrutores/contrutores.js
@@ -1,4 +1,4 @@
-let containerRef = document.querySelector('.posts')
+const containerRef = document.querySelectorAll('.posts');
 
 const itens = [
 
@@ -36,7 +36,7 @@ const itens = [
         img: './imagens/mohg.png',
         titulo: 'Mohg, Lord of Blood',
         descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, eveniet, ipsa soluta debitis sed consectetur temporibus fugit maiores eum labore explicabo voluptates doloremque illo accusantium eius cupiditate aliquid amet facilis!'
-    },
+    }
 
     
 ]
@@ -47,39 +47,112 @@ for (let item of itens)
     containerRef.innerHTML += 
     `
         <div class="item">
+
             <img src="${item.img}">
             <h2>${item.titulo}</h2>
             <p>${item.descricao}</p>
+
         </div>
     `
 
-}
-
-
-
-
+};
 // ================================================
 
+const tituloRef = document.querySelector('#titulo');
+const urlImgRef = document.querySelector('#url');
+const descricaoRef = document.querySelector('#descricao');
+const botaoPostarRef = document.querySelector('#postar') ;
 
+let teste = [];
 
-function Usuario(valorId, valorName,valorIdade,valorAltura,valorPeso){
-  this.id = valorId;
-  this.name = valorName;
-  this.idade = valorIdade;
-  this.altura = valorAltura;
-  this.peso = valorPeso;
-}
+function Post(titulo, urlImg, descricao) {
+    
+    this.tituloRef = titulo;
+    this.urlImg = urlImg;
+    this.descricao = descricao;
 
-const pessoas = [];
+};
 
-function adicionarPessoas()
+function criarPost(titulo, urlImg, descricao) 
 {
-  let pessoa = new Usuario(1,"João",18,1.9,87);
+    //var novoPost = new Post(titulo, urlImg, descricao);
+    var novoPost = new Post(titulo, urlImg, descricao);
+    itens.unshift(novoPost);
+    
+};
+
+//criarPost(tituloRef, urlImgRef, descricaoRef);
+
+//criarPost('Senhor dos Anéis', 'Hobbit', 'Lorem Ipsus');
+
+//console.log(teste);
+
+function validarTitulo(titulo) {
+
+    itens.titulo = titulo
+
 }
-const jose = new Usuario(2,"José",19,1.8,77);
 
-//console.log(joao);
+function validarImg(urlImg) {
 
-pessoas.unshift(joao);
-pessoas.unshift(jose);
- console.log(pessoas);
+    itens.img = urlImg
+
+}
+
+function validarDescricao(descricao) {
+
+    itens.descricao = descricao
+
+
+}
+
+tituloRef.addEventListener('keyup', (event) => validarTitulo(event.target.value));
+
+urlImgRef.addEventListener('keyup', (event) => validarImg(event.target.value));
+
+descricaoRef.addEventListener('keyup', (event) => validarDescricao(event.target.value));
+
+botaoPostarRef.addEventListener('click', (event) => criarPost(event.target.value, event.target.value, event.target.value));
+
+/* CÓDIGO PROFESSSOR IVIN ÚLTIMA AULA
+
+
+window.addEventListener('load', () => console.log('carregou'))
+
+const userEmailRef = document.querySelector('#userEmail')
+const userPasswordRef = document.querySelector('#userPassword')
+const loginButtonRef = document.querySelector('#loginButton')
+
+var userData = {
+    email: '',
+    password: ''
+}
+
+function validarSenha(senha) {
+
+    userData.password = senha
+
+}
+
+
+
+function validarEmail(email) {
+
+    userData.email = email
+
+}
+
+
+
+function login(event) {
+
+    event.preventDefault()
+
+}
+
+userEmailRef.addEventListener('keyup', (event) => validarEmail(event.target.value))
+
+userPasswordRef.addEventListener('keyup', (event) => validarSenha(event.target.value))
+
+loginButtonRef.addEventListener('click', (event) => login(event))
+*/
